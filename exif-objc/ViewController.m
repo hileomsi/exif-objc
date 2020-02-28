e592a5b7b09936c6b62c6b2c1f1ad3379b3b5bbd
Augusto Falcao <falcaoaugustos@gmail.com>
2020-02-28 17:42:49 -0300

    feat(image): update process to use url

--- exif-objc/ViewController.m
+++ exif-objc/ViewController.m
@@ -8,40 +8,48 @@
 
 #import "ViewController.h"
 #import "SYMetadata.h"
+#import <MobileCoreServices/MobileCoreServices.h>
+#import <PhotosUI/PhotosUI.h>
 
 @interface ViewController () <UIImagePickerControllerDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *image;
-@property (weak, nonatomic) IBOutlet UIImageView *image2;
-
 @end
 
 @implementation ViewController
+UIImagePickerController *picker;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker = [[UIImagePickerController alloc] init];
     picker.delegate = self;
+}
+
+- (IBAction)onPressed:(id)sender {
     [self presentViewController:picker animated:YES completion:NULL];
 }
 
-- (void)isolatedFlow:(UIImage *) img {
-//    UIImage *img = [UIImage imageNamed: @"img1_with_exif.jpg"];
-    NSData *data = UIImageJPEGRepresentation(img, 1);
+
+- (void)isolatedFlow:(NSString *) path {
+    NSURL *url = [NSURL fileURLWithPath:path];
+    UIImage *img = [UIImage imageWithContentsOfFile: path];
+    SYMetadata *metadata = [SYMetadata metadataWithFileURL:url];
+
+    // Move it to Main Thread
     self.image.image = img;
-    SYMetadata *metadata = [SYMetadata metadataWithImageData: data];
 
-    NSLog(@"%@", metadata);
-    NSLog(@"\n\n");
+    if (metadata == NULL) {
+        return;
+    }
 
-    if(!metadata.metadataGPS)
+    if (!metadata.metadataGPS)
         metadata.metadataGPS = [[SYMetadataGPS alloc] init];
 
     if (!metadata.metadataIPTC)
         metadata.metadataIPTC = [[SYMetadataIPTC alloc] init];
 
-    metadata.metadataGPS.longitude = @0.0;
-    metadata.metadataGPS.latitude = @0.0;
+    metadata.metadataGPS.longitude = @-38.253;
+    metadata.metadataGPS.latitude = @-3.180;
 
     metadata.metadataExif.dateTimeOriginal = @"2004:08:11 16:45:32";
     metadata.metadataExif.userComment = @"We can add the stringified version of the entry here";
@@ -50,27 +58,46 @@
     metadata.metadataIPTC.city      = @"New York";
     metadata.metadataIPTC.credit    = @"Aloc 2020";
 
-    // create new image data with original image data and edited metadata
-//    NSData *data = UIImageJPEGRepresentation(img, 1);
-    NSData *dataWithMetadata = [SYMetadata dataWithImageData:data andMetadata:metadata];
-    UIImage *newImage = [UIImage imageWithData: dataWithMetadata];
-    self.image2.image = newImage;
+    CGImageDestinationRef imageDestination = CGImageDestinationCreateWithURL((__bridge CFURLRef)url, kUTTypeJPEG, 1, NULL);
 
-    // load metadata for newly cerated image
-    SYMetadata *metadataNew = [SYMetadata metadataWithImageData: dataWithMetadata];
+    if (imageDestination == NULL ) {
+        NSLog(@"Error -> failed to create image destination.");
+        return;
+    }
 
-    UIImageWriteToSavedPhotosAlbum(newImage, NULL, NULL, NULL);
+    CGImageDestinationAddImage(imageDestination, img.CGImage, (__bridge CFDictionaryRef) metadata.generatedDictionary);
 
-    NSLog(@"%@", metadataNew);
+    if (CGImageDestinationFinalize(imageDestination) == NO) {
+        NSLog(@"Error -> failed to finalize the image.");
+    }
+
+    CFRelease(imageDestination);
+
+    [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
+        [PHAssetChangeRequest creationRequestForAssetFromImageAtFileURL:url];
+    } completionHandler: ^(BOOL success, NSError *error) {
+        if (success){
+            NSLog(@"Image Saved!");
+        } else {
+            NSLog(@"Error: %@", error);
+        }
+    }];
 }
 
 #pragma Picker Delegate
 
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<UIImagePickerControllerInfoKey,id> *)info {
     [self dismissViewControllerAnimated:YES completion:NULL];
 
-    UIImage *image = (UIImage *)info[UIImagePickerControllerOriginalImage];
-    [self isolatedFlow:image];
+    NSURL *imageURL = (NSURL *)info[UIImagePickerControllerImageURL];
+    NSString *path = imageURL.path;
+
+    [self isolatedFlow:path];
 }
 
 @end
+
